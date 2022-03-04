11d8794d92b35a7aa4daef45d116187dda6edf10
vdecorges <vincent.decorges@fr.ch>
2022-03-04 15:24:27 +0100

    evol: add updatebag utility and GNU licence normalization

--- bagger.py
+++ bagger.py
@@ -1,7 +1,19 @@
-###
-# Copyright BCU Fribourg 2021
-# Author: nstulz
-###
+ # 
+ # This file is part of the fd_bagger distribution (https://github.com/BCUF/fd_bagger).
+ # Copyright (c) 2022 BCU Fribourg.
+ # 
+ # This program is free software: you can redistribute it and/or modify  
+ # it under the terms of the GNU General Public License as published by  
+ # the Free Software Foundation, version 3.
+ #
+ # This program is distributed in the hope that it will be useful, but 
+ # WITHOUT ANY WARRANTY; without even the implied warranty of 
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ # General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License 
+ # along with this program. If not, see <http://www.gnu.org/licenses/>.
+ #
 
 import logging
 import bagit
